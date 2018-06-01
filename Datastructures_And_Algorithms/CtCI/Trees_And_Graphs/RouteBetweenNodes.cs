5c31ac25d102d3590f0c4e83f3c7f9e658fe0959
pranjay <pranjay.s@gmail.com>
2018-06-01 13:03:48 -0700

    Added Route between nodes

--- Datastructures_And_Algorithms/CtCI/Trees_And_Graphs/RouteBetweenNodes.cs
+++ Datastructures_And_Algorithms/CtCI/Trees_And_Graphs/RouteBetweenNodes.cs
@@ -7,12 +7,15 @@ namespace Datastructures_And_Algorithms.CtCI.Trees_And_Graphs
     {
         public string Name { get; set; }
         public List<Node> Children { get; set; }
+        public NodeState NodeState { get; set; }
 
         public Node(string name)
         {
             Name = name;
         }
     }
+
+    public enum NodeState { Visited, Visiting, Unvisited }
 
     public class Graph
     {
@@ -32,31 +35,49 @@ namespace Datastructures_And_Algorithms.CtCI.Trees_And_Graphs
 
             for (int i = 0; i < 10; i++)
             {
-                g.Nodes.Add(new Node(i.ToString()));
+                var node = new Node(i.ToString()) { NodeState = NodeState.Unvisited };
+                g.Nodes.Add(node);
+
+                for (int j = 0; j < 10; j++)
+                {
+                    g.Nodes[i].Children = new List<Node>();
+                    g.Nodes[i].Children = new List<Node> { new Node(j.ToString()) { NodeState = NodeState.Unvisited } };
+                }
             }
 
             //var hasPath = Search_Dfs(g, g.Nodes[0], g.Nodes[4]);
             //Console.WriteLine($"hasPath DFS={hasPath}");
 
-            var hasPath = Search_Bfs(g, g.Nodes[0], g.Nodes[4]);
+            var hasPath = Search_Bfs(g, g.Nodes[0], g.Nodes[8]);
             Console.WriteLine($"hasPath BFS={hasPath}");
         }
+
 
         public bool Search_Bfs(Graph g, Node root, Node toSearch)
         {
             if (root == null) return root == toSearch;
+
             var q = new Queue<Node>();
             q.Enqueue(root);
+            root.NodeState = NodeState.Visiting;
 
             while (q.Count > 0)
             {
                 var node = q.Dequeue();
-                if (node.Name == toSearch.Name) return true;
+                if (node == null) continue;
 
-                if (node.Children != null && node.Children.Count > 0)
+                if (node.Children == null || node.Children.Count == 0) continue;
+
+                foreach (var n in node.Children)
                 {
-                    foreach (var n in node.Children) { q.Enqueue(n); }
+                    if (n.NodeState == NodeState.Visited) continue;
+                    if (n == toSearch) return true;
+
+                    n.NodeState = NodeState.Visiting;
+                    q.Enqueue(n);
                 }
+
+                node.NodeState = NodeState.Visited;
             }
 
             return false;
