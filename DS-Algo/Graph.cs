9c2892966868843f1bd2b5079bb1b7bb8ea46934
pranjay <pranjay.s@gmail.com>
2018-06-10 14:05:53 -0400

    Added a new project to work on Windows

---
+++ DS-Algo/Graph.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DS_Algo
+{
+    public class Graph
+    {
+        public List<Project> Nodes { get; private set; }
+        public Dictionary<string, Project> Map { get; private set; }
+
+        public Project GetOrCreateNode(string name)
+        {
+            if (!Map.ContainsKey(name))
+            {
+                var project = new Project(name);
+                Nodes.Add(project);
+                Map.Add(name, project);
+            }
+
+            return Map[name];
+        }
+
+        public void AddEdge(string startName, string endName)
+        {
+            var startVertex = GetOrCreateNode(startName);
+            var endVertex = GetOrCreateNode(endName);
+
+            startVertex.AddNeighbor(endVertex);
+        }
+    }
+}
---
+++ DS-Algo/Project.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DS_Algo
+{
+    public class Project
+    {
+        public List<Project> Children { get; private set; }
+        public Dictionary<string, Project> Map { get; private set; }
+        public string Name { get; private set; }
+        public int Dependencies { get; private set; }
+
+        public Project(string name)
+        {
+            this.Name = name;
+        }
+
+        public void AddNeighbor(Project project)
+        {
+            if (Map.ContainsKey(project.Name)) return;
+
+            Children.Add(project);
+            Map.Add(project.Name, project);
+            Dependencies++;
+        }
+    }
+}
